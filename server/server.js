04dc94d7aba3c21edcfc003df08bec4e4d5be254
lydiatie <lydiashinhong-tie@live.nmit.ac.nz>
2021-06-22 19:09:30 +1200

    minor changes

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public/'));
 
     // Handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+    app.get("*", (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 // set port, listen for requests
